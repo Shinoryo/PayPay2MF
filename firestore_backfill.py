0e54456acfffef4a5d14d7e57791eb966cfeb7d4
Shinoryo <shinoryo.studyoutput@gmail.com>
2026-03-30 20:25:38 +0900

    fix: change return type of _run function to None

--- firestore_backfill.py
+++ firestore_backfill.py
@@ -11,7 +11,7 @@ if str(_SRC_DIR) not in sys.path:
     sys.path.insert(0, str(_SRC_DIR))
 
 
-def _run() -> int:
+def _run() -> None:
     main = importlib.import_module("paypay2mf.firestore_backfill").main
     return main()
 
