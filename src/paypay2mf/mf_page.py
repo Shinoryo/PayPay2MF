d3bb437fc5ee1aaf226be842610b0b86089e6e5c
Shinoryo <shinoryo.studyoutput@gmail.com>
2026-04-30 09:08:42 +0900

    import文の修正: SeleniumのChromeオプションとWebDriverのインポートを修正

--- src/paypay2mf/mf_page.py
+++ src/paypay2mf/mf_page.py
@@ -15,7 +15,7 @@ from selenium.common.exceptions import (
     TimeoutException,
     WebDriverException,
 )
-from selenium.webdriver import ActionChains
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions
